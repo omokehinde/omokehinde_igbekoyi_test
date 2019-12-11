aafa33fb2c1fd608b1486005ed19335c8a38ce26
omokehinde <engr.omokehinde@gmail.com>
2019-12-12 00:00:53 +0100

    Question A overlap method

---
+++ README.md
@@ -0,0 +1,4 @@
+<h3>Question A </h3>
+The <b>overlap.py</b> module contains my solution to this question:
+by calling the method overlap(arg1,arg2) passing the 2 points as arguements would 
+return <b>True</b> if the points overlap and <b>False</b> otherwise.
---
+++ overlap.py
@@ -0,0 +1,16 @@
+# Question A
+
+def overlap(p1,p2):
+    points1=[]
+    points2=[]
+    for i in range(p1[0],p1[1]+1):
+        points1.append(i)
+    for i in range(p2[0],p2[1]+1):
+        points2.append(i)
+    for j in points1:
+        if j in points2:
+            return True
+    else:
+        return False
+print(overlap((1,5),(2,6)))
+print(overlap((1,5),(6,8)))
